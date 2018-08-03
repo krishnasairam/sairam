2db80d3e4d9759b7081bc96b42629c7e65d23e54
krishnasairam <sairamadabala007@msitprogram.net>
2018-08-03 17:11:24 +0530

    " -> 3 of 4 passed. pylint: 9.00/10 "

--- cspp1-assignments/m5/p1/perfect_cube.py
+++ cspp1-assignments/m5/p1/perfect_cube.py
@@ -1,13 +1,13 @@
 """python program to find if the given number is a perfect cube or not"""
 def main():
     """perfect cube"""
-    x_in = int(input("Enter an integer:"))
+    x_in = int(input())
     ans_in = 0
     while ans_in**3 < x_in:
         ans_in = ans_in + 1
     if ans_in**3 != x_in:
         print(str(x_in) +" is not a perfect cube")
     else:
-        print("Cube root of "+str(x_in)+" is "+ str(ans_in))
+        print(x_in,"is a perfect cube")
 if __name__ == "__main__":
     main()
