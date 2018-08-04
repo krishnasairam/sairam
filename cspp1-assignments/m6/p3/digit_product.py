270f0545f22f010b7c3fea11030fe8367fe61eef
krishnasairam <sairamadabala007@msitprogram.net>
2018-08-04 11:51:31 +0530

    " -> 0 of 7 passed. pylint: 0/0 "

--- cspp1-assignments/m6/p3/digit_product.py
+++ cspp1-assignments/m6/p3/digit_product.py
@@ -11,10 +11,15 @@ def main():
     int_input = int(input())
     pro_duct = 1
     a_x = 1
-    while int_input != 0:
-        a_x = int_input % 10
-        pro_duct = pro_duct * a_x
-        int_input = int_input // 10
-    print(pro_duct)
+    if int_input == 0:
+        print ('0')
+    else:         
+        while int_input != 0:
+            a_x = int_input % 10
+            pro_duct = pro_duct * a_x
+            int_input = int_input // 10
+        if int_input < 0
+        pro_duct = -pro_duct        
+        print(pro_duct)
 if __name__ == "__main__":
     main()
