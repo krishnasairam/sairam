a4a42c31d9eeaae816fd2b5846b62a2ef94d9828
krishnasairam <sairamadabala007@msitprogram.net>
2018-08-17 17:56:49 +0530

    " -> 0 of 3 passed. pylint: 2.50/10 "

--- cspp1-assignments/CodeCampDocumentDistance/document_distance.py
+++ cspp1-assignments/CodeCampDocumentDistance/document_distance.py
@@ -43,8 +43,8 @@ def main():
     '''
         take two inputs and call the similarity function
     '''
-    input1 = input().lower()
-    input2 = input().lower()
+    input1 = input()
+    input2 = input()
     print(similarity(input1,input2))
 if __name__ == '__main__':
     main()
