212ab0b8c20d6b4240362978ea008ea20ee9eb7a
krishnasairam <sairamadabala007@msitprogram.net>
2018-08-17 14:14:03 +0530

    " -> 1 of 3 passed. pylint: 7.50/10 "

--- cspp1-assignments/CodeCampDocumentDistance/document_distance.py
+++ cspp1-assignments/CodeCampDocumentDistance/document_distance.py
@@ -1,19 +1,14 @@
-'''
-    Document Distance - A detailed description is given in the PDF
-'''
 import math
 FILE = "stopwords.txt"
 def similarity(dict1, dict2):
     '''
         Compute the document distance as given in the PDF
     '''
-    lis3 = dict1+dict2
+    lis3 = dict1 + dict2
     dict3 = {}
     for word in lis3:
-        if word not in load_stopwords(FILE).keys():
-            for i in range(len(word)):
-                if word[i] not in '!@#$%^&*()-+=_?1234567890':
-                    dict3[word] = (dict1.count(word), dict2.count(word))
+      if word not in load_stopwords(FILE).keys():
+        dict3[word] = (dict1.count(word), dict2.count(word))
     num, sum1, sum2 = 0, 0, 0
     for i in dict3:
         num += dict3[i][0] * dict3[i][1]
@@ -37,9 +32,17 @@ def main():
     '''
     input1 = input().lower()
     input2 = input().lower()
-    lis1 = input1.split(' ')
-    lis2 = input2.split(' ')
+    str1 ,str2 = '' , ''
+    for i in input1:
+        if i not in '!@#$%^&*()-+=_?:;.,1234567890'and"'":
+            str1 += i
+    for i in input2:
+        if i not in '!@#$%^&*()-+=_?:;.,1234567890'and"'":
+            str2 += i 
+    lis1 = str1.split(' ')
+    lis2 = str2.split(' ')
     print(similarity(lis1, lis2))
 
 if __name__ == '__main__':
     main()
+
