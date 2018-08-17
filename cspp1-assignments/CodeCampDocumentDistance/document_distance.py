0d0ba6423e75adbac568ca7324e13d9c4eb5a380
krishnasairam <sairamadabala007@msitprogram.net>
2018-08-17 17:46:50 +0530

    " -> 1 of 3 passed. pylint: 4.32/10 "

--- cspp1-assignments/CodeCampDocumentDistance/document_distance.py
+++ cspp1-assignments/CodeCampDocumentDistance/document_distance.py
@@ -2,39 +2,34 @@
     Document Distance - A detailed description is given in the PDF
 '''
 import math
+import re
 stopwords = "stopwords.txt"
-def similarity(dict1, dict2):
+def cleaning(input1):
+    reg = re.compile('[^a-z]')
+    input1 = input1.lower()
+    input1 = [reg.sub(' ',w.strip())for w in input1.split(' ')]
+    return input1
+def remove_stopwords(input1,input2):
+    d = {}
+    wordlist = cleaning(input1) + cleaning(input2)
+    for word in wordlist:
+        if word not in load_stopwords(stopwords).keys():
+            d[word] = (cleaning(input1).count(word),cleaning(input2).count(word))
+    return d        
+
+def similarity(dict1,dict2):
     '''
         Compute the document distance as given in the PDF
     '''
-    list1 = ''
-    for i in dict1:
-        for j in i:
-            if j not in '!@#$%^&*()-+=_?:;.,1234567890':
-                if j not in "'":
-                    list1 += j
-    list2 = ''
-    for i in dict2:
-        for j in i:
-            if j not in '!@#$%^&*()-+=_?:;.,1234567890':
-                if j not in "'":
-                    list2 += j
-
-    list1 = dict1.split()
-    list2 = dict2.split()
-    list3 = list1+list2
-    dict3 = {}
-    for word in list3:
-        if word not in load_stopwords(stopwords).keys():
-            dict3[word] = (dict1.count(word),dict2.count(word))
+    d = {}
+    d = remove_stopwords(dict1,dict2)
     num,sum1,sum2 = 0,0,0  
-    for i in dict3:
-        num  = num + (dict3[i][0] * dict3[i][1])
-        sum1 += dict3[i][0] ** 2
-        sum2 += dict3[i][1] ** 2
+    for i in d:
+        num  += (d[i][0] * d[i][1])
+        sum1 += d[i][0] ** 2
+        sum2 += d[i][1] ** 2
     den = math.sqrt(sum1) * math.sqrt(sum2)
-    similarity = num/den
-    return similarity
+    return num/den
 def load_stopwords(filename):
     '''
         loads stop words from a file and returns a dictionary
@@ -50,6 +45,6 @@ def main():
     '''
     input1 = input().lower()
     input2 = input().lower()
-    print(similarity(input1, input2))
+    print(similarity(input1,input2))
 if __name__ == '__main__':
     main()
