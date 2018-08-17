64a504b21e9b789e522e1b2a6a3d73d2b92a3262
krishnasairam <sairamadabala007@msitprogram.net>
2018-08-17 14:28:21 +0530

    " -> 2 of 3 passed. pylint: 7.37/10 "

--- cspp1-assignments/CodeCampDocumentDistance/document_distance.py
+++ cspp1-assignments/CodeCampDocumentDistance/document_distance.py
@@ -9,13 +9,13 @@ def similarity(dict1, dict2):
     for word in lis3:
       if word not in load_stopwords(FILE).keys():
         dict3[word] = (dict1.count(word), dict2.count(word))
-    num, sum1, sum2 = 0, 0, 0
+    num, sum1, sum2, den = 0, 0, 0, 0
     for i in dict3:
         num += dict3[i][0] * dict3[i][1]
         sum1 += dict3[i][0] ** 2
         sum2 += dict3[i][1] ** 2
     den = math.sqrt(sum1) * math.sqrt(sum2)
-    return (num/den) 
+    return num/den 
 
 def load_stopwords(filename):
     '''
