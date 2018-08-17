2602e698daf0c39b97584ae1f101e25a9756d925
krishnasairam <sairamadabala007@msitprogram.net>
2018-08-17 12:30:34 +0530

    " -> 1 of 3 passed. pylint: 9.35/10 "

--- cspp1-assignments/CodeCampDocumentDistance/document_distance.py
+++ cspp1-assignments/CodeCampDocumentDistance/document_distance.py
@@ -2,25 +2,25 @@
     Document Distance - A detailed description is given in the PDF
 '''
 import math
-stopwords = "stopwords.txt"
+FILE = "stopwords.txt"
 def similarity(dict1, dict2):
     '''
         Compute the document distance as given in the PDF
     '''
     lis3 = dict1+dict2
-    dict3={}
+    dict3 = {}
     for word in lis3:
-        if word not in (load_stopwords(stopwords).keys()):
-        	for i in range(len(word)):
-        	    if word[i] not in '!@#$%^&*()-+=_1234567890':	
-            		dict3[word] = (dict1.count(word),dict2.count(word))
-    num,sum1,sum2 = 0,0,0
+        if word not in load_stopwords(FILE).keys():
+            for i in range(len(word)):
+                if word[i] not in '!@#$%^&*()-+=_?1234567890':
+                    dict3[word] = (dict1.count(word), dict2.count(word))
+    num, sum1, sum2 = 0, 0, 0
     for i in dict3:
-        num += dict3[i][0]*dict3[i][1]
-        sum1 += dict3[i][0]**2
-        sum2 += dict3[i][1]**2
+        num += dict3[i][0] * dict3[i][1]
+        sum1 += dict3[i][0] ** 2
+        sum2 += dict3[i][1] ** 2
     den = math.sqrt(sum1) * math.sqrt(sum2)
-    return round(num/den,1)
+    return num/den
 
 def load_stopwords(filename):
     '''
