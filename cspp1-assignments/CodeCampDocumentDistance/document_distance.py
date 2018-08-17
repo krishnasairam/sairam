1470a60e41f658397178a4d1a2b0462b4764a294
krishnasairam <sairamadabala007@msitprogram.net>
2018-08-17 14:45:01 +0530

    " -> 0 of 3 passed. pylint: 7.50/10 "

--- cspp1-assignments/CodeCampDocumentDistance/document_distance.py
+++ cspp1-assignments/CodeCampDocumentDistance/document_distance.py
@@ -4,11 +4,24 @@ def similarity(dict1, dict2):
     '''
         Compute the document distance as given in the PDF
     '''
+    str1 ,str2 = '' , ''
+    for j in dict1:
+        for i in j:
+            if i not in '!@#$%^&*()-+=_?.,1234567890':
+                if i not in "'":
+                    str1 = str1 + i
+    for j in dict2:
+        for i in j:
+            if i not in '!@#$%^&*()-+=_?.,1234567890':
+                if i not in "'":
+                    str2 = str2 + i 
+    lis1 = str1.split()
+    lis2 = str2.split()
     lis3 = dict1 + dict2
     dict3 = {}
     for word in lis3:
       if word not in load_stopwords(FILE).keys():
-        dict3[word] = (dict1.count(word), dict2.count(word))
+        dict3[word] = (lis1.count(word), lis2.count(word))
     num, sum1, sum2 = 0, 0, 0
     for i in dict3:
         num += dict3[i][0] * dict3[i][1]
@@ -32,20 +45,7 @@ def main():
     '''
     input1 = input().lower()
     input2 = input().lower()
-    str1 ,str2 = '' , ''
-    for j in input1:
-        for i in j:
-            if i not in '!@#$%^&*()-+=_?.,1234567890':
-                if i not in "'":
-                    str1 += i
-    for j in input2:
-        for i in j:
-            if i not in '!@#$%^&*()-+=_?.,1234567890':
-                if i not in "'":
-                    str2 += i 
-    lis1 = str1.split(' ')
-    lis2 = str2.split(' ')
-    print(similarity(lis1, lis2))
+    print(similarity(input1, input2))
 
 if __name__ == '__main__':
     main()
