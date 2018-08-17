5d31f34b1872017bde9218f1f848e91a3a71db1d
krishnasairam <sairamadabala007@msitprogram.net>
2018-08-17 16:15:14 +0530

    " -> 1 of 3 passed. pylint: 7.80/10 "

--- cspp1-assignments/CodeCampDocumentDistance/document_distance.py
+++ cspp1-assignments/CodeCampDocumentDistance/document_distance.py
@@ -1,36 +1,39 @@
+'''
+    Document Distance - A detailed description is given in the PDF
+'''
 import math
-FILE = "stopwords.txt"
+stopwords = "stopwords.txt"
 def similarity(dict1, dict2):
     '''
         Compute the document distance as given in the PDF
     '''
-    str1 ,str2 = '' , ''
-    for j in dict1:
-        for i in j:
-            if i not in '!@#$%^&*()-+=_?.,1234567890':
-                if i not in "'":
-                    str1 = str1 + i
-    for j in dict2:
-        for i in j:
-            if i not in '!@#$%^&*()-+=_?.,1234567890':
-                if i not in "'":
-                    str2 = str2 + i 
-    lis1 = str1.split()
-    lis2 = str2.split()
-    lis3 = dict1 + dict2
-    dict3 = {}
-    for word in lis3:
-      if word not in load_stopwords(FILE).keys():
-        dict3[word] = (lis1.count(word), lis2.count(word))
-    num, sum1, sum2, den = 0, 0, 0, 0
-    for i in dict3:
-        num += dict3[i][0] * dict3[i][1]
-        sum1 += dict3[i][0] ** 2
-        sum2 += dict3[i][1] ** 2
-    den = (math.sqrt(sum1) * math.sqrt(sum2))
-    sim = (num/den) 
-    return sim
+    list1 = ''
+    for i in dict1:
+        for j in i:
+            if j not in '!@#$%^&*()-+=_?:;.,1234567890':
+                if j not in "'":
+                    list1 += j
+    list2 = ''
+    for i in dict2:
+        for j in i:
+            if j not in '!@#$%^&*()-+=_?:;.,1234567890':
+                if j not in "'":
+                    list2 += j
 
+    list1 = dict1.split()
+    list2 = dict2.split()
+    list3 = list1+list2
+    dict3 = {}
+    for word in list3:
+        if word not in load_stopwords(stopwords).keys():
+            dict3[word] = (dict1.count(word),dict2.count(word))
+    num,sum1,sum2 = 0,0,0  
+    for i in dict3:
+        num += dict3[i][0]*dict3[i][1]
+        sum1 += dict3[i][0]**2
+        sum2 += dict3[i][1]**2
+    den = math.sqrt(sum1) * math.sqrt(sum2)
+    return num/den
 def load_stopwords(filename):
     '''
         loads stop words from a file and returns a dictionary
@@ -47,7 +50,5 @@ def main():
     input1 = input().lower()
     input2 = input().lower()
     print(similarity(input1, input2))
-
 if __name__ == '__main__':
     main()
-
