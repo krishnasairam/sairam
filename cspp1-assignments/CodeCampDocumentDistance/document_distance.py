c4b3c1edb50a796be25388c45f31023bcbe92563
krishnasairam <sairamadabala007@msitprogram.net>
2018-08-17 11:35:24 +0530

    " -> 0 of 3 passed. pylint: 6.33/10 "

---
+++ cspp1-assignments/CodeCampDocumentDistance/document_distance.py
@@ -0,0 +1,44 @@
+'''
+    Document Distance - A detailed description is given in the PDF
+'''
+import math
+stopwords = "stopwords.txt"
+def similarity(dict1, dict2):
+    '''
+        Compute the document distance as given in the PDF
+    '''
+    lis3 = dict1+dict2
+    print(lis3)
+    dict3={}
+    for word in lis3:
+        if word not in load_stopwords(stopwords).keys():
+            dict3[word] = (dict1.count(word),dict2.count(word))
+    num,sum1,sum2 = 0,0,0  
+    for i in dict3:
+        num += dict3[i][0]*dict3[i][1]
+        sum1 += dict3[i][0]**2
+        sum2 += dict3[i][1]**2
+    den = math.sqrt(sum1) * math.sqrt(sum2)
+    return round(num/den,1)
+
+def load_stopwords(filename):
+    '''
+        loads stop words from a file and returns a dictionary
+    '''
+    stopwords = {}
+    with open(filename, 'r') as filename:
+        for line in filename:
+            stopwords[line.strip()] = 0
+    return stopwords
+def main():
+    '''
+        take two inputs and call the similarity function
+    '''
+    input1 = input().lower()
+    input2 = input().lower()
+    lis1 = input1.split(' ')
+    lis2 = input2.split(' ')
+    print(similarity(lis1, lis2))
+
+if __name__ == '__main__':
+    main()
