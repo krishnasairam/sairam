62dedb66514b97902025257f7ec5da1af0b7345d
krishnasairam <sairamadabala007@msitprogram.net>
2018-08-23 17:09:48 +0530

    " -> 2 of 2 passed. pylint: 10.00/10 "

--- cspp1-assignments/m20/p1/assignment1.py
+++ cspp1-assignments/m20/p1/assignment1.py
@@ -1,41 +1,42 @@
-# Implement Merge algorithm to sort an integer array in ascending order
+'''Implement Merge algorithm to sort an integer array in ascending order'''
 def merge_sort(alist):
-    if len(alist)>1:
+    '''merge sort of list'''
+    if len(alist) > 1:
         mid = len(alist) // 2
         lefthalf = alist[:mid]
         righthalf = alist[mid:]
         merge_sort(lefthalf)
         merge_sort(righthalf)
-        i=0
-        j=0
-        k=0
+        i = 0
+        j = 0
+        k = 0
         while i < len(lefthalf) and j < len(righthalf):
             if lefthalf[i] < righthalf[j]:
-                alist[k]=lefthalf[i]
-                i=i+1
+                alist[k] = lefthalf[i]
+                i = i+1
             else:
-                alist[k]=righthalf[j]
-                j=j+1
-            k=k+1
+                alist[k] = righthalf[j]
+                j = j+1
+            k = k+1
 
         while i < len(lefthalf):
-            alist[k]=lefthalf[i]
-            i=i+1
-            k=k+1
+            alist[k] = lefthalf[i]
+            i = i+1
+            k = k+1
 
         while j < len(righthalf):
-            alist[k]=righthalf[j]
-            j=j+1
-            k=k+1
+            alist[k] = righthalf[j]
+            j = j+1
+            k = k+1
     return alist
 # Driver code to test above
-num = int(input())
-for _ in range(num):
+NUM_IN = int(input())
+for _ in range(NUM_IN):
     user_input = input()
     list_ = user_input.split()
-    arr=[]
-    for i in list_:
-        arr.append(int(i))
+    arr = []
+    for a in list_:
+        arr.append(int(a))
     # arr = [12, 11, 13, 5, 6, 7]
     merge_sort(arr)
     print(arr)
