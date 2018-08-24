efce61d5e8e31ca8df1099678ae9acf14e28834c
krishnasairam <sairamadabala007@msitprogram.net>
2018-08-24 10:07:30 +0530

    " -> 7 of 14 passed. pylint: 5.00/10 "

---
+++ cspp1-assignments/m21/CodeCampTicTacToe/Tic Tac Toe.py
@@ -0,0 +1,25 @@
+def Tic_Tac_Toe(matrix):
+    winner = []
+    for row in matrix:
+        if row[0] == row[1] == row[2]:
+            winner.append(row[0])
+    for i in range(0,3):        
+        if matrix[0][i] == matrix[1][i] == matrix[2][i]:
+            winner.append(matrix[0][i])
+    if matrix[0][0] == matrix[1][1] == matrix[2][2]:
+            winner.append(matrix[0][i])
+    if matrix[2][0] == matrix[1][1] == matrix[0][2]:
+        winner.append(matrix[0][2])
+    if len(winner) == 1:
+        return winner[0]          
+    else:
+        print("invalid game")
+        return None
+def main():
+    matrix = []
+    for _ in range(0, 3):
+        col = input().split(' ')
+        matrix.append(col)
+    print(Tic_Tac_Toe(matrix))
+if __name__ == '__main__':
+    main()
