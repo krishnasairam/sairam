a54b6e9e07dac17eac3728b6e052284d8b2e5e05
krishnasairam <sairamadabala007@msitprogram.net>
2018-08-25 11:20:11 +0530

    " -> 4 of 4 passed. pylint: 8.50/10 "

--- cspp1-assignments/m22/assignment3/tokenize.py
+++ cspp1-assignments/m22/assignment3/tokenize.py
@@ -3,12 +3,14 @@ Write a function to tokenize a given string and return a dictionary with the fre
 each word
 '''
 def tokenize(list_in):
+    '''tokenize'''
     dict_new = {}
     for i in range(len(list_in)):
         if list_in[i] not in dict_new:
             dict_new[list_in[i]] = list_in.count(list_in[i])
     return dict_new        
 def main():
+    '''input'''
     valid_letters = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789 '
     string_new = ''
     int_input = int(input())
@@ -18,9 +20,7 @@ def main():
         for i in string_in:
             if i in valid_letters:
                 string_new += i
-
     list_in = string_new.split()
-    print(list_in)
-    print(tokenize(list_in))            
+    print(tokenize(list_in))         
 if __name__ == '__main__':
     main()
